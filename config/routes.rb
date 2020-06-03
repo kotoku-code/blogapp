7b76eb68783ce76612ca5109fda5a025262abbd9
kotoku-code <kennedy.kotoku@gmail.com>
2020-06-03 16:02:14 +0200

    installed gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+# Blogger::Application.routes.draw do
   root to: 'articles#index'
   resources :articles do
     resources :comments
+    resources :authors
+
 end
 resources :tags
 end
